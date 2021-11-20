a68a46a9d6b911ca0d3dc9ed568843ecdcbdb72f
mythosmystery <axiysfire@gmail.com>
2021-11-20 17:09:09 -0600

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
+const withPWA = require('next-pwa');
 const isProd = process.env.NODE_ENV === 'production';
 
 /**
  * @type {import('next').NextConfig}
  **/
 
-module.exports = {
+module.exports = withPWA({
    images: {
       loader: 'imgix',
       path: isProd ? 'https://mythosmystery.github.io' : 'http://localhost:3000'
    },
    assetPrefix: isProd ? '/next-gen-weather' : '',
-   basePath: isProd ? '/next-gen-weather' : ''
-};
+   basePath: isProd ? '/next-gen-weather' : '',
+   pwa: {
+      dest: 'public'
+   }
+});
