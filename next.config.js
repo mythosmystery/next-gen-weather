163205c107f6bb2cb62d9ba8af4ff434ecf56334
Hunter Barton <axiysfire@gmail.com>
2022-09-05 02:12:58 -0500

    added docker

--- next.config.js
+++ next.config.js
@@ -1,25 +1,25 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-const isProd = process.env.NODE_ENV === 'production';
+// const withPWA = require('next-pwa');
+// const runtimeCaching = require('next-pwa/cache');
+// const isProd = process.env.NODE_ENV === 'production';
 
-const __basepath__ = '';
+// const __basepath__ = '';
 
-/**
- * @type {import('next').NextConfig}
- **/
+// /**
+//  * @type {import('next').NextConfig}
+//  **/
 
-module.exports = withPWA({
-   images: {
-      loader: 'imgix',
-      path: isProd ? 'https://mythosmystery.github.io' : 'http://localhost:3000'
-   },
-   assetPrefix: isProd ? __basepath__ : '',
-   basePath: isProd ? __basepath__ : '',
-   pwa: {
-      dest: 'public',
-      runtimeCaching,
-      buildExcludes: [/middleware-manifest.json$/],
-      register: true,
-      skipWaiting: true
-   }
-});
+// module.exports = withPWA({
+//    images: {
+//       loader: 'imgix',
+//       path: isProd ? 'https://mythosmystery.github.io' : 'http://localhost:3000'
+//    },
+//    assetPrefix: isProd ? __basepath__ : '',
+//    basePath: isProd ? __basepath__ : '',
+//    pwa: {
+//       dest: 'public',
+//       runtimeCaching,
+//       buildExcludes: [/middleware-manifest.json$/],
+//       register: true,
+//       skipWaiting: true
+//    }
+// });
